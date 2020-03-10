9dff10f44bd2a7a2313cdd8b651843b167df596f
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-03-10 14:11:01 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,12 @@
-def using_times
-	#your code here
-end
+my_age = 23
 
+loop do
+  my_age += 1 
+  puts "Happy Birthday Allen, you are now #{my_age} years old!"
+  if my_age >= 30
+    break
+  end
+end
 
 
 
